b15ab92d7867d4d773d9989e40298c4fd961e433
vroyibg <vroyibg@gmail.com>
2017-10-08 13:36:35 +0700

    add check blocks overlap function

--- brick-breaker/util.js
+++ brick-breaker/util.js
@@ -1,7 +1,22 @@
 // util
 
-Util = new function(){
-    this.distance= function(a,b){
-        return sqrt((a.x-b.x)**2 + (a.y-b.y)**2);
+Util = new function () {
+    this.distance = function (a, b) {
+        return sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+    }
+
+    this.checkIfBlocksOverlap = function (newBlock, blocks) {
+        var flag = false;
+        for(var block of blocks) {
+            if (
+                newBlock.position.x < block.position.x + block.width &&
+                newBlock.position.x + newBlock.width > block.position.x &&
+                newBlock.position.y < block.position.y + block.height &&
+                newBlock.position.y + newBlock.height > block.position.y
+            ) {
+                return true;
+            }
+        };
+        return false;
     }
 }

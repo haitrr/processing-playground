f7347ddc70e23cd7ecb5b519c550ca5a955ecb7d
vroyibg <vroyibg@gmail.com>
2017-10-15 22:16:39 +0700

    add game object class

---
+++ brick-breaker/game-object.js
@@ -0,0 +1,38 @@
+class GameObject {
+
+    constructor(x, y) {
+        this.position = new Point(x, y);
+    }
+
+    get left() {
+
+    }
+
+
+    get right() {
+
+    }
+
+    get bottom() {
+
+    }
+
+
+    get top() {
+
+    }
+
+
+    get center() {
+
+    }
+
+
+    draw() {
+
+    }
+
+    update() {
+
+    }
+}

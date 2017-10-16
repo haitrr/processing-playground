3ec9dd9d7e598c36bda3f359c73858e803503967
vroyibg <vroyibg@gmail.com>
2017-10-16 11:40:35 +0700

    add set methods

--- brick-breaker/objects/regtangle-object.js
+++ brick-breaker/objects/regtangle-object.js
@@ -8,16 +8,32 @@ class RegtangleObject extends GameObject {
         return this.position.x;
     }
 
+    set left(value){
+        this.position.x = value;
+    }
+
     get right() {
         return this.position.x + this.size.width;
+    }
+
+    set right(value){
+        this.position.x = value - this.size.width;
     }
 
     get bottom() {
         return this.position.y + this.size.height;
     }
 
+    set bottom(value){
+        this.position.y = value - this.size.height; 
+    }
+
     get top(){
         return this.position.y;
+    }
+
+    set top(value){
+        this.position.y = value;
     }
 
     draw(){

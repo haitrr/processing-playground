0917d13ba4079cac89fb82b617300c2b0f36f622
vroyibg <vroyibg@gmail.com>
2017-12-02 20:01:41 +0700

    Create class for squares

--- route-finding/sketch.js
+++ route-finding/sketch.js
@@ -7,3 +7,13 @@ function draw() {
     background("white");
   
 }
+
+
+class Square{
+    constructor(x,y,width) {
+        this.width = width;
+        this.color = "blue";
+        this.x = x;
+        this.y = y;
+    }
+}

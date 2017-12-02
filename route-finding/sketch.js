83de13233114000248ac4cee5ef02d5b3e874166
vroyibg <vroyibg@gmail.com>
2017-12-02 20:20:52 +0700

    Update square class

--- route-finding/sketch.js
+++ route-finding/sketch.js
@@ -10,15 +10,31 @@ function draw() {
 
 
 class Square{
-    constructor(x,y,width) {
+    constructor(x,y,width,isWall=fasle) {
         this.width = width;
-        this.color = "blue";
+        this.isWall = isWall;
+        this.distance = 0;
         this.x = x;
         this.y = y;
     }
     draw() {
         stroke("black");
-        fill(this.color);
+        if (this.isWall) {
+            fill("gray");
+        }
+        else {
+            fill("white");
+        }
+        if (this.distance > 0) {
+            stroke("blue");
+            textAlign(CENTER, CENTER);
+            text(this.distance,
+                this.x,
+                this.y,
+                this.width,
+                this.width
+            );
+        }
         rect(this.x, this.y, this, width, this.width);
     }
 }

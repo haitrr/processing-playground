b934cbbeb7f72a0d0bae79b0ed56e4fdcf933407
Hai Van Tran <haitran@2cs.local>
2017-12-04 14:23:05 +0700

    Set start and goal

--- route-finding/sketch.js
+++ route-finding/sketch.js
@@ -1,5 +1,10 @@
 squares = [];
 grid = 25;
+squareWidth = 0;
+hasStart = false;
+finished = false;
+isRunning = false;
+
 function setup() {
     width = window.innerWidth;
     height = window.innerHeight;
@@ -13,7 +18,7 @@ function setup() {
     squareWidth = squareWidth / grid;
     for (var i = 0; i < grid; i++) {
         squares[i] = [];
-        for (var j = 0; j <grid; j++) {
+        for (var j = 0; j < grid; j++) {
             square = new Square(squareWidth * i, squareWidth * j, squareWidth, Math.random() < 0.3);
             squares[i].push(square);
         }
@@ -29,11 +34,27 @@ function draw() {
     }
 }
 
+function mouseClicked() {
+    if (isRunning) return;
+    x = Math.floor(mouseX / squareWidth);
+    y = Math.floor(mouseY / squareWidth);
+    if (squares[x][y].isWall) return;
+    if (!hasStart) {
+        squares[x][y].isStart = true;
+        hasStart = true;
+    } else {
+        squares[x][y].isGoal = true;
+        isRunning = true;
+    }
+
+}
 
 class Square {
-    constructor(x, y, width, isWall = fasle) {
+    constructor(x, y, width, isWall = fasle, isStart = false, isGoal = false) {
         this.width = width;
         this.isWall = isWall;
+        this.isStart = isStart;
+        this.isGoal = isGoal;
         this.distance = 0;
         this.x = x;
         this.y = y;
@@ -43,7 +64,13 @@ class Square {
         if (this.isWall) {
             fill("gray");
         } else {
-            fill("white");
+            if (this.isStart) {
+                fill("green");
+            } else if (this.isGoal) {
+                fill("red");
+            } else {
+                fill("white");
+            }
         }
         if (this.distance > 0) {
             stroke("blue");

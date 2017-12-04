6c2f79c0574544923b3882484d73ec5cda1994cc
Hai Van Tran <haitran@2cs.local>
2017-12-04 14:03:18 +0700

    Create map

--- route-finding/sketch.js
+++ route-finding/sketch.js
@@ -1,16 +1,37 @@
+squares = [];
+grid = 25;
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
-
+    width = window.innerWidth;
+    height = window.innerHeight;
+    createCanvas(width, height);
+    squareWidth = 0;
+    if (width > height) {
+        squareWidth = height;
+    } else {
+        squareWidth = width;
+    }
+    squareWidth = squareWidth / grid;
+    for (var i = 0; i < grid; i++) {
+        squares[i] = [];
+        for (var j = 0; j <grid; j++) {
+            square = new Square(squareWidth * i, squareWidth * j, squareWidth, Math.random() < 0.3);
+            squares[i].push(square);
+        }
+    }
 }
 
 function draw() {
     background("white");
-  
+    for (var i = 0; i < grid; i++) {
+        for (var j = 0; j < grid; j++) {
+            squares[i][j].draw();
+        }
+    }
 }
 
 
-class Square{
-    constructor(x,y,width,isWall=fasle) {
+class Square {
+    constructor(x, y, width, isWall = fasle) {
         this.width = width;
         this.isWall = isWall;
         this.distance = 0;
@@ -21,8 +42,7 @@ class Square{
         stroke("black");
         if (this.isWall) {
             fill("gray");
-        }
-        else {
+        } else {
             fill("white");
         }
         if (this.distance > 0) {
@@ -35,6 +55,6 @@ class Square{
                 this.width
             );
         }
-        rect(this.x, this.y, this, width, this.width);
+        rect(this.x, this.y, this.width, this.width);
     }
 }

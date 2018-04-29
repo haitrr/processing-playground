2486b4c9ca4f22bff0964bb08893a8594391f669
vroyibg <vroyibg@gmail.com>
2018-04-29 22:40:19 +0700

    add eslint, draw tiles

--- 2048/sketch.js
+++ 2048/sketch.js
@@ -1,45 +1,74 @@
-let grid = [];
-let size = 4;
-let width = 800;
-let height = 640;
+let grid = []
+let size = 4
+let tileWidth = 100
+let width = 800
+let height = 640
 
-function setup() {
-    createCanvas(width, height);
-    setupGrid();
+/* eslint-disable no-unused-vars */
+function setup () {
+  /* eslint-enable no-unused-vars */
+  createCanvas(width, height)
+  setupGrid()
+  addNumber()
+  addNumber()
+  addNumber()
+  addNumber()
+  addNumber()
 }
 
-function draw() {
-    background(0);
+/* eslint-disable no-unused-vars */
+function draw () {
+  /* eslint-enable no-unused-vars */
+  createCanvas(width, height)
+  background(255)
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      noFill()
+      strokeWeight(2)
+      stroke(0)
+      let x = i * tileWidth
+      let y = j * tileWidth
+      rect(x, y, tileWidth, tileWidth)
+      let value = grid[i][j]
+      if (value != null && value !== 0) {
+        textAlign(CENTER, CENTER)
+        fill(0)
+        noStroke()
+        textSize(tileWidth / 2)
+        text(value, x, y, tileWidth, tileWidth)
+      }
+    }
+  }
 }
 
 // get blank tiles
-function getBlank() {
-    let blanks = [];
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            if (grid[i][j] == 0) {
-                blanks.push({
-                    x: i,
-                    y: j
-                });
-            }
-        }
+function getBlank () {
+  let blanks = []
+  for (var i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      if (grid[i][j] === 0) {
+        blanks.push({
+          x: i,
+          y: j
+        })
+      }
     }
-    return blanks;
+  }
+  return blanks
 }
 
-function addNumber() {
-    blanks = getBlank();
-    tile = random(blanks);
-    grid[tile.x][tile.y] = random([2, 4]);
+function addNumber () {
+  let blanks = getBlank()
+  let tile = random(blanks)
+  grid[tile.x][tile.y] = random([2, 4])
 }
 
-function setupGrid() {
-    for (let i = 0; i < size; i++) {
-        row = [];
-        for (let j = 0; j < size; j++) {
-            row.push(0);
-        }
-        grid.push(row);
+function setupGrid () {
+  for (let i = 0; i < size; i++) {
+    let row = []
+    for (let j = 0; j < size; j++) {
+      row.push(0)
     }
+    grid.push(row)
+  }
 }
